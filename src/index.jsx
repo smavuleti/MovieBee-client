219b30f41f91c34e2dd2fa59b869cd59bd5f0c08
Sruthi Mavuleti <mavuleti160@gmail.com>
2024-12-21 12:49:43 -0800

    Resolved github errors

--- src/index.jsx
+++ src/index.jsx
@@ -1,33 +1,20 @@
 import { createRoot } from "react-dom/client";
-<<<<<<< Updated upstream
-=======
 import { MainView } from "./components/main-view/main-view.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from 'react-bootstrap/Container';
 
 
->>>>>>> Stashed changes
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 import { Container } from "react-bootstrap";
 
 // Main component (will eventually use all the others)
 const MovieBeeApp = () => {
-<<<<<<< Updated upstream
-
-  return (
-    <div className ="movie-bee">
-      <div> Welcome to Movie Bee Application!</div>
-    </div>
-  )
-
-=======
   return (
   <Container >
     <MainView />
   </Container> 
   );
->>>>>>> Stashed changes
 };
 
 // Finds the root of your app
