042c250fcf49aefd7aec7b603aceb63e909c1350
Sruthi Mavuleti <mavuleti160@gmail.com>
2024-12-21 10:33:40 -0800

    Added React Bootstrap
    Focused on Container, Cards, UI Elements, Images, Colorography and Typography

---
+++ src/components/movie-card/movie-card.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+import { Card } from "react-bootstrap";
+
+export const MovieCard = ({ movie, onMovieClick }) => {
+  return (
+    <Card style={{ width: "18rem" }} bg="dark" text="light">
+      <Card.Img variant="top" src={movie.ImagePath} />
+      <Card.Body>
+        <Card.Title>{movie.MovieTitle}</Card.Title>
+        <Card.Subtitle className="mb-2">{movie.MovieGenre}</Card.Subtitle>
+        <Card.Link style={{ cursor: "pointer" }} onClick={() => onMovieClick(movie)}>View Details</Card.Link>
+      </Card.Body>
+    </Card>
+  );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    MovieTitle: PropTypes.string,
+    MovieDirector: PropTypes.string,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
+};
